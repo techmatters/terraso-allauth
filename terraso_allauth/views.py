b4e450537e3d2f8296e158c70118ef9567272124
Caio Carrara <caio@techmatters.org>
2022-04-04 18:45:59 -0300

    chore: use generic OAuth callback view directly

--- terraso_allauth/views.py
+++ terraso_allauth/views.py
@@ -1,14 +1,10 @@
 import requests
-from allauth.socialaccount.helpers import complete_social_login, render_authentication_error
-from allauth.socialaccount.models import SocialLogin
-from allauth.socialaccount.providers.base import ProviderException
-from allauth.socialaccount.providers.base.constants import AuthError
-from allauth.socialaccount.providers.oauth2.client import OAuth2Error
-from allauth.socialaccount.providers.oauth2.views import OAuth2Adapter, OAuth2LoginView, OAuth2View
-from allauth.utils import get_request_param
+from allauth.socialaccount.providers.oauth2.views import (
+    OAuth2Adapter,
+    OAuth2CallbackView,
+    OAuth2LoginView,
+)
 from django.conf import settings
-from django.core.exceptions import PermissionDenied
-from requests import RequestException
 
 from .provider import TerrasoProvider
 
@@ -49,45 +45,4 @@ class TerrasoOAuth2Adapter(OAuth2Adapter):
 
 
 oauth2_login = OAuth2LoginView.adapter_view(TerrasoOAuth2Adapter)
-
-
-class OAuth2CallbackView(OAuth2View):
-    def dispatch(self, request, *args, **kwargs):
-        if "error" in request.GET or "code" not in request.GET:
-            auth_error = request.GET.get("error", None)
-
-            if auth_error == self.adapter.login_cancelled_error:
-                error = AuthError.CANCELLED
-            else:
-                error = AuthError.UNKNOWN
-
-            return render_authentication_error(request, self.adapter.provider_id, error=error)
-
-        app = self.adapter.get_provider().get_app(self.request)
-        client = self.get_client(self.request, app)
-
-        try:
-            access_token = self.adapter.get_access_token_data(request, app, client)
-            token = self.adapter.parse_token(access_token)
-            token.app = app
-            login = self.adapter.complete_login(request, app, token, response=access_token)
-            login.token = token
-
-            if self.adapter.supports_state:
-                login.state = SocialLogin.verify_and_unstash_state(
-                    request, get_request_param(request, "state")
-                )
-            else:
-                login.state = SocialLogin.unstash_state(request)
-
-            return complete_social_login(request, login)
-        except (
-            PermissionDenied,
-            OAuth2Error,
-            RequestException,
-            ProviderException,
-        ) as e:
-            return render_authentication_error(request, self.adapter.provider_id, exception=e)
-
-
 oauth2_callback = OAuth2CallbackView.adapter_view(TerrasoOAuth2Adapter)
